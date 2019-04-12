599490d836fd4eeb5c0cb68e80b9d2153c014cf2
Saneri <santeri.suitiala@aalto.fi>
2019-04-12 12:45:47 +0200

    Add shared memory allocation and SIGINT handler
    Signed-off-by: Saneri <santeri.suitiala@aalto.fi>

--- src/simulador.c
+++ src/simulador.c
@@ -1,3 +1,10 @@
+/*
+ * @file simulador.c
+ * @author Santeri Suitiala & Roberto Pirck Valdés, grupo 5, practicas 2212
+ * @date
+ * @brief 
+ */
+
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -13,15 +20,65 @@
 
 #include <mapa.h>
 
+#define SHM_MAPA "/shm_mapa_SOPER"
 
 int main() {
 	int ret=0;
 	exit(ret);
 }
 
-// Inicializar la simulación
+// Manojador de la señal SIGTERM (ctrl + C)
+void manejador_SIGINT(int sig) {
+
+}
+
+/*
+ * @brief Inicializar la simulación
+ * @return 0 si todo ha sido bien, -1 en el caso de error
+ */
 int init() {
-	return -1; // no implementado
+
+	struct sigaction act;
+	int fd_shm;
+	tipo_mapa* mapa;
+
+	// Inicializar el manejador para SIGINT
+	act.sa_handler = manejador_SIGINT;
+	int error = sigaction(SIGINT, &act, NULL);
+	if (error < 0) {
+		perror("No se pudo agregar el manejador a SIGINT");
+		return -1;
+	}
+
+	// Inicializar memoria compartida para la mapa
+	fd_shm = shm_open(SHM_MAPA, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+	if (fd_shm < 0) {
+		perror("(shm_open) No se pudo inicializar la memoria compartida");
+		return -1;
+	}
+	
+	// Map la memoria compartida para el uso de este proceso  
+	mapa = mmap(NULL, sizeof(tipo_mapa), PROT_WRITE | PROT_READ, MAP_SHARED, fd_shm, 0);
+	if (mapa == MAP_FAILED) {
+		perror("(mmap) No se pudo mapear la memoria compartid de mapa");
+		return -1;
+	}
+	
+	// Crear procesos jefes
+	pid_t pid;
+	for (int i=0; i<N_EQUIPOS; i++) {
+		pid = fork();
+		if (pid < 0) {
+			perror("(fork) No se pudo crear nuevo proceso");
+			return -1;
+		} else if (pid == 0) {
+			// Jefe (proceso hijo)
+		} else {
+			// Simulador (proceso padre)
+		}
+	}
+
+	return 0;
 }
 
 // Intenta a atacar a una posicion con la nave elegido
@@ -38,5 +95,3 @@ int nave_mover(tipo_nave *nave) {
 int nave_destruir(tipo_nave *nave) {
 	return -1; // no implementado
 }
-
-
