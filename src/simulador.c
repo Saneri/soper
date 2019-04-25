d641af1ef5ae0ae9b537ef72c931a987dd53025a
vivarob <robertopirck@gmail.com>
2019-04-25 09:24:41 +0200

    changes on map simulacion arguments

--- src/simulador.c
+++ src/simulador.c
@@ -35,36 +35,36 @@ int nave_atacar(tipo_nave *nave, int targety, int targetx) {
 //	1 ---- E
 //	2 ---- S
 //	3 ---- W
-int nave_mover(tipo_nave *nave, int targety, int targetx){
-	mapa_clean_casilla(tipo_mapa *mapa, nave->posy, nave->posx);
-	mapa_set_nave(tipo_mapa *mapa, tipo_nave nave);
+int nave_mover(tipo_mapa *mapa, tipo_nave *nave, int targety, int targetx){
+	mapa_clean_casilla(mapa, nave->posy, nave->posx);
+	mapa_set_nave(mapa, tipo_nave nave);
 }
 
-int nave_mover_aleatorio(tipo_nave *nave) {
+int nave_mover_aleatorio(tipo_mapa *mapa, tipo_nave *nave) {
 	int direccion = rand() % 4;
 	bool selected = false;
 	while(!selected){}
 		switch(direccion) {
 			case 0:
-				if (mapa_is_casilla_vacia(tipo_mapa *mapa, nave->posy + 1, nave->posx){
+				if (mapa_is_casilla_vacia(mapa, nave->posy + 1, nave->posx){
 					nave_mover(nave, nave->posy + 1, nave->posx);
 					selected = true;
 				}
 				break;
 			case 1:
-				if (mapa_is_casilla_vacia(tipo_mapa *mapa, nave->posy, nave->posx + 1){
+				if (mapa_is_casilla_vacia(mapa, nave->posy, nave->posx + 1){
 					nave_mover(nave, nave->posy, nave->posx + 1);
 					selected = true;
 				}
 				break;
 			case 2:
-				if (mapa_is_casilla_vacia(tipo_mapa *mapa, nave->posy - 1, nave->posx){
+				if (mapa_is_casilla_vacia(mapa, nave->posy - 1, nave->posx){
 					nave_mover(nave, nave->posy - 1, nave->posx);
 					selected = true;
 				}
 				break;
 			case 3:
-				if (mapa_is_casilla_vacia(tipo_mapa *mapa, nave->posy, nave->posx - 1){
+				if (mapa_is_casilla_vacia(mapa, nave->posy, nave->posx - 1){
 					nave_mover(nave, nave->posy, nave->posx - 1);
 					selected = true;
 				}
@@ -78,10 +78,10 @@ int nave_mover_aleatorio(tipo_nave *nave) {
 }
 
 // Destruye una nave librando sus recursos
-int nave_destruir(tipo_nave *nave) {
+int nave_destruir(tipo_mapa *mapa, tipo_nave *nave) {
 	nave->vida = 0;
 	nave->viva = false;
-	mapa_clean_casilla(tipo_mapa *mapa, nave->posy, nave->posx);
+	mapa_clean_casilla(mapa, nave->posy, nave->posx);
 	return -1; // no implementado
 }
 
